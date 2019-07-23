b7707e19aae34d2cb03832a87e55993046df6a38
KitchenLab6 <KitchenLab6@TEKCENTER.LOCAL>
2019-07-23 10:05:07 -0700

    Added OG Memes

---
+++ OG Memes.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Og Memes</title>
+</head>
+<body>
+   <div class="container">
+        <ul class="nav nav-tabs">
+                <li class="nav-item">
+                  <a class="nav-link active" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="daily_memes.php">Daily Memes</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="OG Memes.php">Og Memes</a>
+                </li>
+                <li class="nav-item">
+                </li>
+              </ul>
+<body>
+
+              <img src="https://ih1.redbubble.net/image.469301443.9861/pp,550x550.u4.jpg" alt="">
+
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSThwQ61RpfsvH-XnNiAfVH3QsknrXrBaHhUvOSEFxT7F_TFRjR" alt="">
+</body>
+</html>
---
+++ daily_memes.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Daily Memes</title>
+</head>
+<div class="container">
+        <ul class="nav nav-tabs">
+                <li class="nav-item">
+                  <a class="nav-link active" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="daily_memes.php">Daily Memes</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="OG Memes.php">Og Memes</a>
+                </li>
+                <li class="nav-item">
+                </li>
+              </ul>
+
+<img src="https://i.imgur.com/XQAEvnR.jpg" alt="">
+
+
+             
+<body>
+    
+<img src="https://pm1.narvii.com/6709/8529a6a766711a9f5e905e257cdced7e7e3c52e3_hq.jpg" alt="">
+
+
+
+
+
+
+
+</body>
+</html>
